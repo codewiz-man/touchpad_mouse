9792ff43f87e273d47046e1dd8fc0f36c5a1696e
Manjunath <manjunathls13@gmail.com>
2023-08-20 16:41:42 +0530

    touchpad code fixed

--- server/websocket_server.py
+++ server/websocket_server.py
@@ -12,16 +12,17 @@ SERVER_IP = ""
 SERVER_PORT = 2023
 SERVER_QRCODE = b""
 
-TOUCHSTART = 0x00
-TOUCHMOVE = 0x01
-TOUCHEND = 0x02
-LEFT_BTN_PRESS = 0x03
-LEFT_BTN_RELEASE = 0x04
-RIGHT_BTN_PRESS = 0x05
-RIGHT_BTN_RELEASE = 0x06
-SCROLL = 0x07
-LEFT_CLICK = 0x08
-RIGHT_CLICK = 0x09
+TOUCHSTART = "00"
+TOUCHMOVE = "01"
+TOUCHEND = "02"
+LEFT_BTN_PRESS = "03"
+LEFT_BTN_RELEASE = "04"
+RIGHT_BTN_PRESS = "05"
+RIGHT_BTN_RELEASE = "06"
+SCROLL = "07"
+LEFT_CLICK = "08"
+RIGHT_CLICK = "09"
+DBL_CLICK = "10"
 
 DELIMITER = "_"
 
@@ -38,7 +39,7 @@ async def echo(websocket):
         #await websocket.send(message)
         print(message)
         msg = message.split(DELIMITER)
-        cmd = int(msg[0])
+        cmd = msg[0]
         
         if cmd == TOUCHSTART:
             mouse.press(Button.left)
@@ -48,13 +49,16 @@ async def echo(websocket):
         elif cmd == TOUCHMOVE:
             #x =  twos_complement( str(msg[1]), 8 )
             #y = twos_complement( str(msg[2]), 8 )
-            x = int(msg[1])
-            y = int(msg[2])
+            x = float(msg[1])
+            y = float(msg[2])
             print(x, y)
             mouse.move(x,y)
         elif cmd == LEFT_CLICK:
             print("left click")
             mouse.click(Button.left)
+        elif cmd == RIGHT_CLICK:
+            print("right click")
+            mouse.click(Button.right)
         elif cmd == LEFT_BTN_PRESS:
             print("left press")
             mouse.press(Button.left)
@@ -67,6 +71,9 @@ async def echo(websocket):
         elif cmd == RIGHT_BTN_RELEASE:
             print("right release")
             mouse.release(Button.right)
+        elif cmd == DBL_CLICK:
+            print("dbl click")
+            mouse.click(Button.left, 2)
         elif cmd == SCROLL:
             print("scroll")
             sc = int(msg[1])
